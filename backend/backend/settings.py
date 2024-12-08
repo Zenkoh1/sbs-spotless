a482cb21375206bf34679b0621d63e23ece30d5c
Zen Koh <zenkoh193@gmail.com>
2024-12-08 23:05:08 +0800

    Add secret key to .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#b6*4l54n*=6rw&6^)u3y%w9)nd$xvfcg-v92pj9srsuj+4q(('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
