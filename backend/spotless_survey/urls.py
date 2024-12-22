fdc58a644dca81059e65985e4e46d14ad4b63a32
Zen Koh <zenkoh193@gmail.com>
2024-12-22 23:59:25 +0800

    Add survey routes by bus

--- backend/spotless_survey/urls.py
+++ backend/spotless_survey/urls.py
@@ -7,5 +7,7 @@ router.register('survey', views.CleanlinessSurveyViewSet, basename='survey')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('survey/<int:bus_id>/get_survey_by_bus/', views.CleanlinessSurveyViewSet.as_view({'get': 'get_survey_by_bus'}), name='get_survey_by_bus'),
+    path('survey/<int:bus_id>/resolve_all_survey_by_bus/', views.CleanlinessSurveyViewSet.as_view({'patch': 'resolve_all_survey_by_bus'}), name='resolve_all_survey_by_bus'),
 ]
 
--- backend/spotless_survey/views.py
+++ backend/spotless_survey/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import CleanlinessSurvey, CleanlinessSurveyImage
 from .serializer import CleanlinessSurveySerializer
+from rest_framework.decorators import action
 
 # Create your views here.
 class CleanlinessSurveyViewSet(viewsets.ModelViewSet):
@@ -10,7 +11,7 @@ class CleanlinessSurveyViewSet(viewsets.ModelViewSet):
     serializer_class = CleanlinessSurveySerializer
     permission_classes = []
     authentication_classes = []
-    http_method_names = ['get', 'post']
+    http_method_names = ['get', 'post', 'patch']
     
     def perform_create(self, serializer):
         cleanliness_survey = serializer.save()
@@ -19,3 +20,26 @@ class CleanlinessSurveyViewSet(viewsets.ModelViewSet):
                 survey=cleanliness_survey,
                 image=image
             )
+
+    # only non resolved surveys
+    @action(detail=False, methods=['get'])
+    def get_survey_by_bus(self, request, *args, **kwargs):
+        bus_id = self.kwargs.get('bus_id')
+        if bus_id is None:
+            return Response("Bus ID is required", status=status.HTTP_400_BAD_REQUEST)
+        surveys = CleanlinessSurvey.objects.filter(bus_id=bus_id, is_resolved=False)
+
+        serializer = CleanlinessSurveySerializer(surveys, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['patch'])
+    def resolve_all_survey_by_bus(self, request, *args, **kwargs):
+        bus_id = self.kwargs.get('bus_id')
+        if bus_id is None:
+            return Response("Bus ID is required", status=status.HTTP_400_BAD_REQUEST)
+        surveys = CleanlinessSurvey.objects.filter(bus_id=bus_id)
+        for survey in surveys:
+            survey.is_resolved = True
+            survey.save()
+        return Response("All surveys resolved", status=status.HTTP_200_OK)
+
