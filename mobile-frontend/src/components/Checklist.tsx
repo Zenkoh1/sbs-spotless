83636f3483d7ec7fcec488b7cee68e85f3fc487a
brendanng7 <brendanngjj@gmail.com>
2024-12-24 22:29:24 +0800

    Add patching of status of steps as IN_PROGRESS

--- mobile-frontend/src/components/Checklist.tsx
+++ mobile-frontend/src/components/Checklist.tsx
@@ -14,10 +14,30 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
 import { Link as RouterLink } from "react-router-dom";
 import { CleaningChecklistStep_Backend_Type } from "../types/CleaningSchedule.type";
+import { BACKEND_URL } from "../constants";
+import axios from "axios";
 
 export default function Checklist(props: {
   checklist: CleaningChecklistStep_Backend_Type[];
 }) {
+  const { checklist } = props;
+  const updateStatus = async (step_id: number) => {
+    try {
+      const response = await axios.patch(
+        `${BACKEND_URL}checklist_steps/${step_id}/`,
+        {
+          status: "IN_PROGRESS",
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <Box>
       <TableContainer component={Paper}>
@@ -30,8 +50,8 @@ export default function Checklist(props: {
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.checklist &&
-              props.checklist.map((item) => (
+            {checklist &&
+              checklist.map((item) => (
                 <TableRow key={item.id}>
                   <TableCell>{item.cleaning_checklist_item.title}</TableCell>
                   <TableCell>
@@ -48,6 +68,7 @@ export default function Checklist(props: {
                   <TableCell>
                     {item.status !== "COMPLETE" && (
                       <Link
+                        onClick={() => updateStatus(item.id)}
                         component={RouterLink}
                         to={`scan/${item.id}`}
                         underline="hover"
